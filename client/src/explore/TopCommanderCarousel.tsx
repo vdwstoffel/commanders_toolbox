ab5c825cbce845f26aef39b98af0ed7a3ec6e11b
Stoffel <86371768+vdwstoffel@users.noreply.github.com>
2025-07-23 20:07:36 +0200

    add click handler to show cardinfo

--- client/src/explore/TopCommanderCarousel.tsx
+++ client/src/explore/TopCommanderCarousel.tsx
@@ -1,7 +1,10 @@
+import FullCardInfo from "@/components/cards/FullCardInfo";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import ErrorMessage from "@/components/ui/ErrorMessage";
 import Loader from "@/components/ui/Loader";
+import OverlayWrapper from "@/components/ui/OverlayWrapper";
 import { useGetTopCommanders } from "@/hooks/useExploreQuery";
+import { useState } from "react";
 
 interface TopCommanderProps {
   period: "year" | "month" | "week";
@@ -9,9 +12,21 @@ interface TopCommanderProps {
 
 export default function TopCommanderCourasel({ period }: TopCommanderProps) {
   const { isLoadingTopCommander, topCommanderError, topCommanderData } = useGetTopCommanders(period);
+  const [showCardInfo, setShowCardInfo] = useState<boolean>(false);
+  const [cardInfoToShow, setCardInfoToShow] = useState<string | null>(null);
 
   if (isLoadingTopCommander) return <Loader />;
   if (topCommanderError) return <ErrorMessage msg={topCommanderError.message} />;
+
+  function toggleCardInfo(cardName?: string) {
+    setShowCardInfo(!showCardInfo);
+
+    if (cardInfoToShow === null) {
+      setCardInfoToShow(cardName!);
+    } else {
+      setCardInfoToShow(null);
+    }
+  }
 
   return (
     <div className="flex flex-col mx-20 border border-amber-50">
@@ -19,7 +34,7 @@ export default function TopCommanderCourasel({ period }: TopCommanderProps) {
         <CarouselContent>
           {topCommanderData!.map((card) => {
             return (
-              <CarouselItem key={card.id} className="basis-1/10">
+              <CarouselItem key={card.id} onClick={() => toggleCardInfo(card.cardName)} className="basis-1/10">
                 <img className="w-60" src={card.cardImageUrl[0]} />
               </CarouselItem>
             );
@@ -28,6 +43,11 @@ export default function TopCommanderCourasel({ period }: TopCommanderProps) {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
+      {showCardInfo && (
+        <OverlayWrapper hideFn={toggleCardInfo}>
+          <FullCardInfo cardName={cardInfoToShow!} />
+        </OverlayWrapper>
+      )}
     </div>
   );
 }
