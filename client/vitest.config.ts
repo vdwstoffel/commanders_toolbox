f099d1047c7769e702a4d0dc91e225def34b081f
Stoffel <86371768+vdwstoffel@users.noreply.github.com>
2025-07-31 18:44:17 +0200

    initial unit tests

---
+++ client/vitest.config.ts
@@ -0,0 +1,19 @@
+
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
