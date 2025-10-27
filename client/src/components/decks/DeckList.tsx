f8d9a8c1734434b6c8d7986b7dd153a19af5ea9b
Stoffel <86371768+vdwstoffel@users.noreply.github.com>
2025-10-27 22:21:04 +0100

    remove unused hook import

--- client/src/components/decks/DeckList.tsx
+++ client/src/components/decks/DeckList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 import MagicCardImage from "../cards/MagicCardImage";
