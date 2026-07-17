e8e83fb4cedcd626ff62395d22838d558b422020
Stoffel <86371768+vdwstoffel@users.noreply.github.com>
2026-07-17 12:27:59 +0200

    client: retint theme to Obsidian & Gold dark, add Cinzel/Inter fonts, enable dark mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/cinzel/500.css";
+import "@fontsource/cinzel/700.css";
+import "@fontsource/cinzel/900.css";
+
 import "./index.css";
 import App from "./App.tsx";
 import UserContextProvider from "./components/user/UserContextProvider.tsx";
@@ -17,7 +25,11 @@ createRoot(document.getElementById("root")!).render(
     <ReactQueryDevtools initialIsOpen={false} />
     <UserContextProvider>
       <App />
-      <Toaster />
+      <Toaster
+        toastOptions={{
+          style: { background: "#1b1a17", color: "#ece6d8", border: "1px solid #33302a" },
+        }}
+      />
     </UserContextProvider>
   </QueryClientProvider>
 );
